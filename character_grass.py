21fe864341bda2fa6ff347dc47316447c7ceb470
Jirung-E <jh10590326@gmail.com>
2023-09-21 11:52:11 +0900

    무한루프 확인

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,28 @@
 from pico2d import *
+import math
 
 open_canvas()
 
-# fill here
+character = load_image("character.png")
+grass = load_image("grass.png")
+
+def runCircle():
+    print("Circle")
+    pass
+
+def runRect():
+    print("Rect")
+    pass
+
+
+
+while True:
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+
+    runCircle()
+    runRect()
+
+    delay(0.01)
 
 close_canvas()
